4a5a709f29fd4eaefa7a06dcc422018173a2655b
Luizrrl21 <luiz@masterrepr.com.br>
2023-11-14 17:28:38 -0300

    att

--- python/aulaExtra_selenium/leitor.py
+++ python/aulaExtra_selenium/leitor.py
@@ -1,10 +1,10 @@
 #Bibliotecas
 import os
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pyttsx3
-
 
 #Limpando terminal
 os.system('cls')
@@ -14,19 +14,16 @@ opcoes = Options()
 opcoes.add_experimental_option("detach", True)
 
 #Browser
-navegador = webdriver.Chrome(options=opcoes) #Abrir navegador com configuração Ativada
+navegador = webdriver.Chrome(ChromeDriverManager().install()) #Abrir navegador com configuração Ativada
 navegador.maximize_window() #Máximizar tela
-navegador.get("https://www.contosdeterror.site/") #Abrir site
-
-#Selecionar história
-leia_mais = navegador.find_element(By.XPATH, '//*[@id="Blog1"]/div[1]/article[2]/div/div/div[4]/div[2]/a') #Inspecionar -> Copy XPATH
-leia_mais.click()
+navegador.get("https://ge.globo.com/futebol/brasileirao-serie-a/") #Abrir site
 
 #Paragrafos
-paragrafo1 = navegador.find_element(By.XPATH, '//*[@id="post-body-4073905748094003101"]/p[10]/span')
+paragrafo1 = navegador.find_element(By.XPATH, '//*[@id="classificacao__wrapper"]/article/section[1]/div/table[1]/tbody/tr[1]/td[2]/strong')
 
 #Narração
 narrador = pyttsx3.init()
 narrador.setProperty("rate", 150)
+narrador.say("O líder do campeonato é o:")
 narrador.say(paragrafo1.text)
 narrador.runAndWait()
