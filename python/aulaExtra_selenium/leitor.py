17f08108b3ae78389ad58d7fa0d8073d9c8fb48d
Luizrrl21 <luiz@masterrepr.com.br>
2023-11-15 07:59:41 -0300

    att

--- python/aulaExtra_selenium/leitor.py
+++ python/aulaExtra_selenium/leitor.py
@@ -20,10 +20,13 @@ navegador.get("https://ge.globo.com/futebol/brasileirao-serie-a/") #Abrir site
 
 #Paragrafos
 paragrafo1 = navegador.find_element(By.XPATH, '//*[@id="classificacao__wrapper"]/article/section[1]/div/table[1]/tbody/tr[1]/td[2]/strong')
+paragrafo2 = navegador.find_element(By.XPATH, '//*[@id="classificacao__wrapper"]/article/section[1]/div/table[1]/tbody/tr[2]/td[2]/strong')
 
 #Narração
 narrador = pyttsx3.init()
-narrador.setProperty("rate", 150)
+narrador.setProperty("rate", 250)
 narrador.say("O líder do campeonato é o:")
 narrador.say(paragrafo1.text)
+narrador.say("O vice é o")
+narrador.say(paragrafo2.text)
 narrador.runAndWait()
