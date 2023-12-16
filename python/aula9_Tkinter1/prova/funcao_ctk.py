4a88e5a1bb7115ac596a8e557d38bcd9a51f7ba0
Luizrrl21 <luiz@masterrepr.com.br>
2023-12-16 20:29:14 -0300

    att

--- python/aula9_Tkinter1/prova/funcao_ctk.py
+++ python/aula9_Tkinter1/prova/funcao_ctk.py
@@ -8,11 +8,11 @@ def conv_metro(valor, app):
         numero = float(valor)
         metro = customtkinter.CTkLabel(text=f"{numero/100} m", master=app, text_color="green")
         metro.grid(row=3,column=0)
-        app.after(2000, lambda: delete_message(metro))
+        app.after(3000, lambda: delete_message(metro))
     except:
         metro = customtkinter.CTkLabel(text="Digite um número!", master=app, text_color="red")
         metro.grid(row=3,column=0)
-        app.after(2000, lambda: delete_message(metro))
+        app.after(3000, lambda: delete_message(metro))
 
 
 def conv_cent(valor, app):
@@ -20,8 +20,8 @@ def conv_cent(valor, app):
         numero = float(valor)
         cent = customtkinter.CTkLabel(text=f"{numero*100} cm",master=app, text_color="green")
         cent.grid(row=3,column=1)
-        app.after(2000, lambda: delete_message(cent))
+        app.after(3000, lambda: delete_message(cent))
     except:
         metro = customtkinter.CTkLabel(text="Digite um número!", master=app, text_color="red")
         metro.grid(row=3,column=1)
-        app.after(2000, lambda: delete_message(metro))
+        app.after(3000, lambda: delete_message(metro))
--- python/aula9_Tkinter1/prova/funcao_tk.py
+++ python/aula9_Tkinter1/prova/funcao_tk.py
@@ -8,19 +8,19 @@ def conv_metro(valor, app):
         numero = float(valor)
         metro = Label(text=f"{numero/100} m",master=app, bg="black", fg="green")
         metro.grid(row=3,column=0)
-        app.after(2000, lambda: delete_message(metro))
+        app.after(3000, lambda: delete_message(metro))
     except:
         metro = Label(text="Digite um número",master=app, bg="black", fg="red")
         metro.grid(row=3,column=0)
-        app.after(2000, lambda: delete_message(metro))
+        app.after(3000, lambda: delete_message(metro))
 
 def conv_cent(valor, app):
     try:
         numero = float(valor)
         cent = Label(text=f"{numero*100} cm",master=app, bg="black", fg="green")
         cent.grid(row=3,column=1)
-        app.after(2000, lambda: delete_message(cent))
+        app.after(3000, lambda: delete_message(cent))
     except:
         metro = Label(text="Digite um número",master=app, bg="black", fg="red")
         metro.grid(row=3,column=1)
-        app.after(2000, lambda: delete_message(metro))
+        app.after(3000, lambda: delete_message(metro))
