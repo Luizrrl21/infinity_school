14082143ac7d1734ba41fbb633ca099de140d425
Luiz Roberto <145810784+Luizrrl21@users.noreply.github.com>
2024-01-26 18:58:57 -0300

    Add files via upload

--- python/aula12_POO2/desafio_pratico.py
+++ python/aula12_POO2/desafio_pratico.py
@@ -1,53 +1,50 @@
 class Conta:
-    def __init__(self, num_conta:int, titular:str, saldo:float):
-        self.num_conta = num_conta
-        self.titular = titular
-        self.saldo = saldo
+    def __init__(self, numero:int,titular:str,saldo:float):
+       self.numero = numero
+       self.titular = titular
+       self.saldo = saldo
 
-    def depositar(self):
-        valor = float(input("Valor do Depósito: R$"))
-        self.saldo = valor + self.saldo
-        print(f"Saldo atual: R${self.saldo}")
+    def depositar(self, valor):
+        self.saldo += valor
         return self.saldo
-    
-    def sacar(self):
-        valor = float(input("Valor do Saque: R$"))
-        if valor > self.saldo:
-            print("Saldo Insuficiente!")
+
+    def sacar(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            return self.saldo
         else:
-            self.saldo = self.saldo - valor
-        print(f"Saldo atual: R${self.saldo}")
+            print("Saldo insuficiente")
+
+    def ver_saldo(self):
         return self.saldo
-    
-    def verSaldo(self):
-        print(f"Saldo atual: R${self.saldo}")
-        return self.saldo
+
+    def resumo(self):
+        return f"""
+    Informações da conta:
+    Número da conta: {self.numero}
+    Titular da conta: {self.titular}
+    Saldo da conta: {self.saldo}
+"""
+
 
 class ContaCorrente(Conta):
-    def __init__(self, num_conta: int, titular: str, saldo: float, taxa_manutencao:float, limite_cheque: float):
-        super().__init__(num_conta, titular, saldo)
+    def __init__(self, numero: int, titular: str, saldo: float, taxa_manutencao:float, limite_cheque:float):
+        super().__init__(numero, titular, saldo)
         self.taxa_manutencao = taxa_manutencao
         self.limite_cheque = limite_cheque
+    
+    def sacar(self, valor):
+            if self.saldo + self.limite_cheque >= valor:
+                self.saldo = self.saldo - valor
+                return self.saldo
+            else:
+                print("Saldo insuficiente")
 
-    def sacar(self):
-        valor = float(input("Valor do Saque: R$"))
-        if valor > self.saldo and valor < self.limite_cheque:
-            while True:
-                cheque = str(input(f"Saldo Insuficiente! Deseja usar Cheque Especial? (Limite R${self.limite_cheque}) (Y/n)"))
-                if cheque == "n":
-                    break
-                elif cheque == "Y":
-                    self.limite_cheque = self.limite_cheque - valor
-                    print(f"Saldo atual: R${self.saldo}")
-                    print(f"Limite Cheque: R${self.limite_cheque}")
-                    break
-                else:
-                    print("Erro! Digite apenas 'Y' para sim ou 'n' para não")
-        else:
-            self.saldo = self.saldo - valor
-            print(f"Saldo atual: R${self.saldo}")
-        return self.saldo
+class ContaPoupanca(Conta):
+    def __init__(self, numero: int, titular: str, saldo: float, taxa_juros:float):
+        super().__init__(numero, titular, saldo)
+        self.taxa_juros = taxa_juros
 
-
-user = ContaCorrente(num_conta=123, titular="Luiz Roberto", saldo=0.0, taxa_manutencao=0.5, limite_cheque=1000)
-user.verSaldo()
+    def calcular_juros(self):
+        valor_adicional = self.saldo * self.taxa_juros
+        self.saldo += valor_adicional
